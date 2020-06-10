50a18201af923283abe6b811b277e22400d4516e
sauravk7077 <sauravk7077@gmail.com>
2020-06-10 07:00:14 +0530

    added linter

--- login_system/users/form.py
+++ login_system/users/form.py
@@ -1,8 +1,10 @@
 from flask_wtf import FlaskForm
-from wtforms.validators import DataRequired, Length, Email
-from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms import StringField, SubmitField, PasswordField
 
 class RegistrationForm(FlaskForm):
-    username = StringField(label="username",validators=[DataRequired(),Length(min=4, max=5)])
-    email = StringField(label="email", validators=[DataRequired(), Email()])
+    username = StringField(label="Username", render_kw={"placeholder": "Enter your username"},validators=[DataRequired(),Length(min=4, max=5)])
+    email = StringField(label="Email", validators=[DataRequired(), Email()])
+    password = PasswordField(label="Password", validators=[DataRequired(), Length(min=8)])
+    confirm_password = PasswordField(label="Confirm Password", validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField(label="Submit")
