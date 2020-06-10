7b2683e89a0f3e8b7f19244d978ae1a55a342bbc
sauravk7077 <sauravk7077@gmail.com>
2020-06-10 08:06:48 +0530

    updated the registration form

--- login_system/users/form.py
+++ login_system/users/form.py
@@ -6,7 +6,9 @@ class RegistrationForm(FlaskForm):
     username = StringField(label="Username", \
         render_kw={"placeholder": "Enter your username"},validators=[DataRequired(),Length(min=4, max=5)])
     email = StringField(label="Email",\
-        validators=[DataRequired(), Email()])
-    password = PasswordField(label="Password", validators=[DataRequired(), Length(min=8)])
-    confirm_password = PasswordField(label="Confirm Password", validators=[DataRequired(), EqualTo('password')])
+        render_kw={"placeholder":"Enter your email"}, validators=[DataRequired(), Email()])
+    password = PasswordField(label="Password",\
+        render_kw={"placeholder":"Enter your password"}, validators=[DataRequired(), Length(min=8)])
+    confirm_password = PasswordField(label="Confirm Password",\
+        render_kw={"placeholder":"Enter your password again."}, validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField(label="Submit")
