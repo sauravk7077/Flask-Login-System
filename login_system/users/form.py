b9a5126aa5afd93cc451fe6397b24aa1669e80f4
sauravk7077 <sauravk7077@gmail.com>
2020-06-10 07:07:09 +0530

    added linter

--- login_system/users/form.py
+++ login_system/users/form.py
@@ -3,8 +3,10 @@ from wtforms.validators import DataRequired, Length, Email, EqualTo
 from wtforms import StringField, SubmitField, PasswordField
 
 class RegistrationForm(FlaskForm):
-    username = StringField(label="Username", render_kw={"placeholder": "Enter your username"},validators=[DataRequired(),Length(min=4, max=5)])
-    email = StringField(label="Email", validators=[DataRequired(), Email()])
+    username = StringField(label="Username", \
+        render_kw={"placeholder": "Enter your username"},validators=[DataRequired(),Length(min=4, max=5)])
+    email = StringField(label="Email",\
+        validators=[DataRequired(), Email()])
     password = PasswordField(label="Password", validators=[DataRequired(), Length(min=8)])
     confirm_password = PasswordField(label="Confirm Password", validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField(label="Submit")
--- login_system/users/routes.py
+++ login_system/users/routes.py
@@ -1,13 +1,15 @@
 from flask.blueprints import Blueprint
-from flask import render_template;
+from flask import render_template, redirect, url_for;
 from login_system.users.form import RegistrationForm
 
 users = Blueprint('users', __name__)
 
 
 
-@users.route('/register')
+@users.route('/register', methods=["POST","GET"])
 def register():
     form = RegistrationForm()
+    if form.validate_on_submit():
+        return redirect(url_for('main.home'))
     return render_template('Register.html', title="Register", form=form)
 
